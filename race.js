5d93fea81cf45e0c8cdfcfd9e3468a7636cd2cb4
eks197 <eks197@users.noreply.github.com>
2018-05-06 21:12:01 -0500

    fixed the race.

--- race.js
+++ race.js
@@ -1,59 +1,48 @@
 //JavaScript for race to work
 
-var msu = document.getElementById('bear');
-var mizzou = document.getElementById('tiger');
+var msu = document.getElementById('msu');
+var mizzou = document.getElementById('mizzou');
 var placeB;
 var placeT;
-var msu_pic = document.getElementById('msuPic');
-var mizzou_pic = document.getElementById('mizzouPic');
-var lightR =document.getElementById('redlight');
-var lightG =document.getElementById('greenlight');
+var lightR = document.getElementById('redlight');
+var lightG = document.getElementById('greenlight');
 
-function lightOn() 
-{
-lightR.style.display = 'none';
-lightG.style.display = 'block';
+function lightOn() {
+  lightR.style.display = 'none';
+  lightG.style.display = 'block';
 }
 
-function initialize()
-{
+function initialize() {
 
-placeB = 0;
-placeT = 0;
-msu.style.left = placeB + 'px';
-mizzou.style.left = placeT + 'px';
-msu_pic.src = "msu_bear.png";
-mizzou_pic.src = "mizzou_tiger.png";
+  placeB = 0;
+  placeT = 0;
+  msu.style.paddingLeft = placeB + '%';
+  mizzou.style.paddingLeft = placeT + '%';
 
-lightOn();
+  lightOn();
 }
 
-function startRace()
-{
-initialize();
+function startRace() {
+  initialize();
 
-var timer = setInterval(frame, 100);
-function frame() 
-{
-    if (placeB > 500 || placeT > 500 ){
+  var timer = setInterval(frame, 100);
+  function frame() {
+    if (placeB > 100 || placeT > 100) {
       clearInterval(timer);
-      posB > posG ? msuWin() : mizzouWin();
+      placeB > placeT ? msuWin() : mizzouWin();
     } else {
-      var randB = Math.floor(Math.random() * 15);
-      var randT = Math.floor(Math.random() * 15);
-      placeB += randB;
-      placeT += randT;
-      msu.style.left = placeB + 'px';
-      mizzou.style.left = placeT + 'px';
-
+      placeB += Math.floor(Math.random() * 15);
+      placeT += Math.floor(Math.random() * 15);
+      msu.style.paddingLeft = placeB + '%';
+      mizzou.style.paddingLeft = placeT + '%';
     }
-
-function msuWin ()
-{
-window.alert("Go Bears!");
+  }
 }
 
-function mizzouWin()
-{
-window.alert("Dang it Mizzou Won :(");
+function msuWin() {
+  window.alert("Go Bears!");
+}
+
+function mizzouWin() {
+  window.alert("Dang it Mizzou Won :(");
 }
